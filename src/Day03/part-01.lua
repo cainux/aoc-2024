e95616a2e0f8d2ce47902acc228986988552859e
Cain Voong <cainux@gmail.com>
2024-12-03 20:27:06 +0000

    🎄 Day 03 🎄

---
+++ src/Day03/part-01.lua
@@ -0,0 +1,24 @@
+local file = io.open("input.txt", "r")
+
+if file == nil then
+	print("error opening file")
+	os.exit(1)
+end
+
+function Matches(inputstr, expression)
+	local matches = {}
+	for match in inputstr:gmatch(expression) do
+		table.insert(matches, match)
+	end
+	return matches[1], matches[2]
+end
+
+local input = file:read("a")
+local acc = 0
+
+for mul in input:gmatch("mul%(%d+,%d+%)") do
+	local l, r = Matches(mul, "%d+")
+	acc = acc + (tonumber(l) * tonumber(r))
+end
+
+print(acc)
---
+++ src/Day03/part-02.lua
@@ -0,0 +1,34 @@
+local rex = require("rex_pcre2")
+local file = io.open("input.txt", "r")
+
+if file == nil then
+	print("error opening file")
+	os.exit(1)
+end
+
+function Matches(inputstr, expression)
+	local matches = {}
+	for match in inputstr:gmatch(expression) do
+		table.insert(matches, match)
+	end
+	return matches[1], matches[2]
+end
+
+local input = file:read("a")
+local doThing = true
+local acc = 0
+
+for match in rex.gmatch(input, "mul\\([\\d,]+?\\)|do\\(\\)|don't\\(\\)") do
+	if match == "do()" then
+		doThing = true
+	elseif match == "don't()" then
+		doThing = false
+	else
+		if doThing then
+			local l, r = Matches(match, "%d+")
+			acc = acc + (tonumber(l) * tonumber(r))
+		end
+	end
+end
+
+print(acc)
