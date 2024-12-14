84a537d1e069808c6c6415687287f028e3570032
Cain Voong <cainux@gmail.com>
2024-12-14 14:26:46 +0000

    wip

--- src/Day12/part-01.lua
+++ src/Day12/part-01.lua
@@ -33,11 +33,28 @@ end
 local function char_at(g, x, y) return g[y][x] end
 
 local function score(grid)
+    local plots = {}
     for y = 1, #grid do
         for x = 1, #grid[y] do
-            print(x, y, char_at(grid, x, y))
+            table.insert(plots, { x = x, y = y, char = char_at(grid, x, y) })
+
+            -- print(x, y, char_at(grid, x, y))
+            --
+            -- local nesw = {
+            --     { x = x, y = y - 1 },
+            --     { x = x, y = y + 1 },
+            --     { x = x - 1, y = y },
+            --     { x = x + 1, y = y },
+            -- }
         end
     end
+    -- Get all the plots into a list
+    -- get the first plot in the list, start a new 'region'
+    -- get all the plots around it, if they are the same char, add them to the region, remove them from the list
+    -- get all the plots around the plots in the region, if they are the same char, add them to the region
+    -- repeat until no more plots can be added to the region
+    -- get the next plot in the list, start a new region
+    -- repeat until all plots have been added to a region
 end
 
 -- solve
