ef3b8d0d1f9dd7f4a8cfc3f02f39210c060b7097
Cain Voong <cainux@gmail.com>
2024-12-20 17:18:56 +0000

    wip

--- src/Day12/part-01.lua
+++ src/Day12/part-01.lua
@@ -27,7 +27,7 @@ local function load(input)
         table.insert(grid, {})
         local x = 1
         for char in line:gmatch "." do
-            table.insert(grid[#grid], char)
+            table.insert(grid[#grid], { x = x, y = y, plot = char })
             table.insert(plots, { x = x, y = y, plot = char })
             x = x + 1
         end
@@ -38,48 +38,65 @@ end
 
 local function char_at(g, x, y) return g[y][x].plot end
 
-local function score(grid, plots)
+local function surrounding_plots(g, x, y)
+    local max_x, max_y = #g[1], #g
+    local nesw = {
+        { x = x, y = y - 1 },
+        { x = x, y = y + 1 },
+        { x = x - 1, y = y },
+        { x = x + 1, y = y },
+    }
+    local plots = {}
+    for _, p in ipairs(nesw) do
+        if p.x > 0 and p.x <= max_x and p.y > 0 and p.y <= max_y then
+            table.insert(plots, g[p.y][p.x])
+        end
+    end
+    return plots
+end
+
+local function is_in(plots, plot)
+    for _, p in ipairs(plots) do
+        if p.x == plot.x and p.y == plot.y then
+            return true
+        end
+    end
+    return false
+end
+
+local function score_grid(grid, plots)
     local regions = {}
+
     while #plots > 0 do
         local region = {}
-        local plot = table.remove(plots, 1)
-        table.insert(region, plot)
-        local nesw = {
-            { x = plot.x, y = plot.y - 1 },
-            { x = plot.x, y = plot.y + 1 },
-            { x = plot.x - 1, y = plot.y },
-            { x = plot.x + 1, y = plot.y },
-        }
-        for _, p in ipairs(nesw) do
+        local queue = {}
+        local curr = table.remove(plots, 1)
+        table.insert(queue, curr)
 
+        while #queue > 0 do
+            local p = table.remove(queue, 1)
+            local surr = surrounding_plots(grid, p.x, p.y)
+            for _, s in ipairs(surr) do
+                if is_in(plots, s) and s.plot == p.plot then
+                    table.insert(region, s)
+                    table.insert(queue, s)
+                    for i, q in ipairs(plots) do
+                        if s.x == q.x and s.y == q.y then
+                            table.remove(plots, i)
+                            break
+                        end
+                    end
+                end
+            end
         end
+
         table.insert(regions, region)
     end
 
     print(#regions)
-
-    -- for y = 1, #grid do
-    --     for x = 1, #grid[y] do
-    -- print(x, y, char_at(grid, x, y))
-    --
-    -- local nesw = {
-    --     { x = x, y = y - 1 },
-    --     { x = x, y = y + 1 },
-    --     { x = x - 1, y = y },
-    --     { x = x + 1, y = y },
-    -- }
-    --     end
-    -- end
-    -- Get all the plots into a list
-    -- get the first plot in the list, start a new 'region'
-    -- get all the plots around it, if they are the same char, add them to the region, remove them from the list
-    -- get all the plots around the plots in the region, if they are the same char, add them to the region
-    -- repeat until no more plots can be added to the region
-    -- get the next plot in the list, start a new region
-    -- repeat until all plots have been added to a region
 end
 
 -- solve
 local grid, plots = load(content)
 -- print_grid(grid)
-score(grid, plots)
+score_grid(grid, plots)
