8612e2e41f887a4a46527b3a37a7d225c13c0740
Cain Voong <cainux@gmail.com>
2024-12-24 21:38:31 +0000

    wip

--- src/Day14/part-01.lua
+++ src/Day14/part-01.lua
@@ -32,7 +32,42 @@ local function print_grid(robots)
         end
         io.write "\n"
     end
+
+    print()
 end
 
+local function move(robots)
+    for _, robot in ipairs(robots) do
+        robot.px = robot.px + robot.vx
+        robot.py = robot.py + robot.vy
+
+        -- If the robot has moved out of max_x or max_y then teleport
+        while robot.px < 0 do
+            robot.px = robot.px + max_x
+        end
+
+        while robot.px > max_x do
+            robot.px = robot.px - max_x
+        end
+
+        while robot.py < 0 do
+            robot.py = robot.py + max_y
+        end
+
+        while robot.py > max_y do
+            robot.py = robot.py - max_y
+        end
+    end
+end
+
+-- Solve
 local robots = load(file:lines())
+file:close()
+
 print_grid(robots)
+
+for i = 1, 5 do
+    move(robots)
+    print("After " .. i .. " seconds:")
+    print_grid(robots)
+end
